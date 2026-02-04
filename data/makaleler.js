e93e713f75bcf21edd1d95796119a7388988793f
refikysl <refik@kku.edu.tr>
2026-02-04 04:29:19 +0300

    Create makaleler.js

---
+++ data/makaleler.js
@@ -0,0 +1,131 @@
+const makalelerData = [
+    {
+        "yazar": "Yaslıkaya, R., ve Yozgat, D.",
+        "yil": "2025",
+        "baslik": "Müşterilerin Kafe Tercihlerini Etkileyen Faktörler: Kırıkkale İli Yahşihan İlçesi Yenişehir Mahallesi Örneği",
+        "dergi": "Ekonomi İşletme Siyaset ve Uluslararası İlişkiler Dergisi",
+        "cilt": "11",
+        "sayi": "1",
+        "sayfa": "30-50",
+        "doi": ""
+    },
+    {
+        "yazar": "Şahan, M., ve Yaslıkaya, R.",
+        "yil": "2025",
+        "baslik": "Kentte Yaşayanların Boş Zaman Etkinlikleri Üzerine Bir Değerlendirme: Hobi Bahçeleri Örneği",
+        "dergi": "Kırıkkale Üniversitesi Sosyal Bilimler Dergisi",
+        "cilt": "15",
+        "sayi": "2",
+        "sayfa": "206-222",
+        "doi": ""
+    },
+    {
+        "yazar": "Arslan, F. D., ve Yaslıkaya, R.",
+        "yil": "2024",
+        "baslik": "Soylulaştırmanın Dönüşümü: Devlet Eliyle Soylulaştırma",
+        "dergi": "Ankara Hacı Bayram Veli Üniversitesi İktisadi ve İdari Bilimler Fakültesi Dergisi",
+        "cilt": "26",
+        "sayi": "2",
+        "sayfa": "673-700",
+        "doi": ""
+    },
+    {
+        "yazar": "Yazıcı, M., ve Yaslıkaya, R.",
+        "yil": "2021",
+        "baslik": "Bir Yerel Yönetim Sorumluluğu Olarak Kent Güvenliği: Balıkesir İli Bandırma İlçesi Örneği",
+        "dergi": "Van Yüzüncü Yıl Üniversitesi İktisadi ve İdari Bilimler Fakültesi Dergisi",
+        "cilt": "6",
+        "sayi": "12",
+        "sayfa": "78-92",
+        "doi": ""
+    },
+    {
+        "yazar": "Yaslıkaya, R.",
+        "yil": "2019",
+        "baslik": "Yerel Yönetimlerde Ölçeği Büyütmenin Siyasi Sonuçları: Avrupa'da Belediye Birleşmeleri Üzerinden Bir İnceleme",
+        "dergi": "Amme İdaresi Dergisi",
+        "cilt": "52",
+        "sayi": "1",
+        "sayfa": "33-65",
+        "doi": ""
+    },
+    {
+        "yazar": "Yaslıkaya, R.",
+        "yil": "2019",
+        "baslik": "Yerel Yönetimlerde Ölçeği Büyütmenin Ekonomik Sonuçları: Belediye Birleşmeleri Üzerinden Bir İnceleme",
+        "dergi": "Turkish Studies Social Science",
+        "cilt": "14",
+        "sayi": "5",
+        "sayfa": "2683-2706",
+        "doi": ""
+    },
+    {
+        "yazar": "Yaslıkaya, R.",
+        "yil": "2019",
+        "baslik": "Yerel Yönetimlerde Ölçek Ekonomisi: Teori, Kavram ve Tartışmalar",
+        "dergi": "Liberal Düşünce",
+        "cilt": "24",
+        "sayi": "95",
+        "sayfa": "163-192",
+        "doi": "10.36484/liberal.612182"
+    },
+    {
+        "yazar": "Pustu, Y., Abdulai, A., ve Yaslıkaya, R.",
+        "yil": "2019",
+        "baslik": "Political, Economic and Cultural Dimensions of Corruption: A Case of Developed and Developing Countries",
+        "dergi": "Üçüncü Sektör Sosyal Ekonomi",
+        "cilt": "54",
+        "sayi": "3",
+        "sayfa": "1377-1398",
+        "doi": ""
+    },
+    {
+        "yazar": "Yaslıkaya, R.",
+        "yil": "2019",
+        "baslik": "Katı Atık Hizmetlerinde Özelleştirme: Taşeron İşçilerin Kadroya Geçişi Bir Fırsat mıdır Tehdit mi?",
+        "dergi": "Üçüncü Sektör Sosyal Ekonomi Dergisi",
+        "cilt": "54",
+        "sayi": "3",
+        "sayfa": "1319-1342",
+        "doi": ""
+    },
+    {
+        "yazar": "Yaslıkaya, R.",
+        "yil": "2015",
+        "baslik": "Ekolojik Paradigmada Yeni Bir Kavşak Çevreci Sinema",
+        "dergi": "International Journal of Sport Culture and Science",
+        "cilt": "3",
+        "sayi": "1",
+        "sayfa": "410-428",
+        "doi": "10.14486/IJSCS308"
+    },
+    {
+        "yazar": "Yaylı, H., ve Yaslıkaya, R.",
+        "yil": "2015",
+        "baslik": "İnsan Doğa İlişkisi Tasarımında Radikal Dönüşüm: Derin Ekoloji",
+        "dergi": "International Journal of Sport Culture and Science",
+        "cilt": "3",
+        "sayi": "1",
+        "sayfa": "452-465",
+        "doi": "10.14486/IJSCS312"
+    },
+    {
+        "yazar": "Yaylı, H., ve Yaslıkaya, R.",
+        "yil": "2012",
+        "baslik": "Türkiye'de Yerel Hizmetlerle İlgili Yasal Düzenlemelerin Gelişimi",
+        "dergi": "Üçüncü Sektör Sosyal Ekonomi",
+        "cilt": "47",
+        "sayi": "2",
+        "sayfa": "65-82",
+        "doi": ""
+    },
+    {
+        "yazar": "Bülbül, K., ve Yaslıkaya, R. (2006",
+        "baslik": "Sivil Topluma Siyasal Güven: Türk Milletvekillerinin Sivil Toplum Örgütlerine Güven Düzeyi Üzerine Bir Araştırma",
+        "dergi": "Sivil Toplum Düşünce ve Araştırma Dergisi",
+        "cilt": "4",
+        "sayi": "16",
+        "sayfa": "91-105",
+        "doi": ""
+    }
+];
