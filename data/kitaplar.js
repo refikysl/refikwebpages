a9a09e0aac5f9906ce9903e13ce6f28cce49a599
refikysl <refik@kku.edu.tr>
2026-02-04 15:55:46 +0300

    Create kitaplar.js

---
+++ data/kitaplar.js
@@ -0,0 +1,62 @@
+const kitaplarData = {
+    "kitaplar": [
+        {
+            "yazar": "Yaslıkaya, R.",
+            "yil": "2025",
+            "baslik": "Metropoliten Yönetim: Teori, Model ve Uygulama",
+            "yayin": "Ankara: Liberte",
+            "link": "kitaplar/Metropoliten_Yonetim_Teori_Model_ve_Uygu.pdf"
+        }
+    ],
+    "bolumler": [
+        {
+            "yazar": "Yaslıkaya, R.",
+            "yil": "2025",
+            "baslik": "Local Governments in Unitary and Federal States",
+            "yayin": "In H. Yaylı (Ed.), Local governments and local diplomacy practices.",
+            "link": ""
+        },
+        {
+            "yazar": "Yaslıkaya, R.",
+            "yil": "2018",
+            "baslik": "Recent Developments in The Turkish Local Government System in The Context Of Centralization and Decentralization Debates (2012–2018)",
+            "yayin": "In H. Özüdoğru, M. Mulalıç, ve T. Çetin (Eds.), Economic and Social Studies (ss. 240–263). Riga: Lambert Academic Publishing.",
+            "link": "kitaplar/Recent_Developments_in_The_Turkish_Local.pdf"
+        },
+        {
+            "yazar": "Yaslıkaya, R., ve Arslan, F. D.",
+            "yil": "2023",
+            "baslik": "Uluslararası Çevre Politikaları",
+            "yayin": "In M. Yaman, Ö. Önder, ve H. Şengün (Eds.), Çevre Yönetimi ve Politikaları (ss. 224–252). Bursa: Ekin Yayınevi.",
+            "link": "kitaplar/Uluslararasi_Cevre_Politikalari.pdf"
+        },
+        {
+            "yazar": "Yaslıkaya, R.",
+            "yil": "2021",
+            "baslik": "Türk Kamu Yönetiminin Temel Kavramları",
+            "yayin": "In H. Yaylı (Ed.), Türk Kamu Yönetimi (ss. 21–69). Ankara: Orion.",
+            "link": "kitaplar/KAMU_YONETIMININ_TEMEL_KAVRAMLARI.pdf"
+        },
+        {
+            "yazar": "Yaylı, H., ve Yaslıkaya, R.",
+            "yil": "2019",
+            "baslik": "Kent ve Göç",
+            "yayin": "In M. A. Özer (Ed.), Kent ve … (ss. 141–161). Ankara: Gazi Kitabevi.",
+            "link": "kitaplar/Kent ve göç.pdf"
+        },
+        {
+            "yazar": "Pustu, Y., ve Yaslıkaya, R.",
+            "yil": "2019",
+            "baslik": "Yönetim ve Etik",
+            "yayin": "In M. A. Özer (Ed.), Yönetim ve … (ss. 63–85). Ankara: Gazi Kitabevi.",
+            "link": "kitaplar/Yönetim ve Etik.pdf"
+        },
+        {
+            "yazar": "Yücel Batmaz, N., ve Yaslıkaya, R.",
+            "yil": "2019",
+            "baslik": "Yönetim ve Göç",
+            "yayin": "In M. A. Özer (Ed.), Yönetim ve … (ss. 89–111). Ankara: Gazi Kitabevi.",
+            "link": "kitaplar/yönetim ve göç.pdf"
+        }
+    ]
+};
