56a3fe829a5d4714c8d1e020e7713e0eaf866350
refikysl <refik@kku.edu.tr>
2026-02-07 04:02:47 +0300

    Create bildiriler.js

---
+++ data/bildiriler.js
@@ -0,0 +1,58 @@
+const bildirilerData = [
+    {
+        "citation": `Yaslıkaya, R., ve Tandırcı, M. A. (2025). Stratejiden Gerçekliğe: Türkiye’nin Ulusal Yapay Zekâ Stratejisi̇ Üzerine Eleştirel Bir Değerlendirme. Türk Dünyasında Karşılaştırmalı Kamu Yönetimi, 160-164. Kazakistan Türkistan.`,
+        "link": "bildiriler/türkiye yapay zeka stratejisi.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R., ve Yozgat, D. (2025). Birlikler Eliyle Atık Yönetim Sistemi̇: Kırıkkale Özelinde Yaşanan Sorunlar ve Çözüm Önerileri̇. Karmaşık Sorunlar Karşısında Kamu Yönetimi: Yaklaşımlar, Politikalar ve Uygulamalar, 919-924. Trabzon: Karadeniz Teknik Üniversitesi. `,
+        "link": "bildiriler/Birlikler_Eliyle_Atik_Yonetim_Sistemi_Ki.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R., ve Yozgat, D. (2024). Kırıkkale Hizmet Sektörü Analizi̇: Yahşihan İlçesi̇ Yenişehir Mahallesi̇ Kafe Sektörü. TR71 Bölgesinin Sosyo-Ekonomisi Sempozyumu, 36-37. Kırıkkale: Kırıkkale Üniversitesi.`,
+        "link": "bildiriler/kırıkkale hizmet sektörü analizi.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R., ve Aslan, F. D. (2024). Türkiye’de 2019 ve 2024 Yerel Seçimlerinde Seçilen Belediye Başkanlarının Karşılaştırılmasına Yönelik Bir Çalışma. Ahi Evran 4. International Congress on Scientific Research, 785-786. Kırşehir: Ahi Evran Üniversitesi.`,
+        "link": "bildiriler/Turkiye_de_2019_Ve_2024_Yerel_Secimlerin.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R., ve Nişli, S. G. (2024). Yerel Siyasetin Esnaflaşması: Kırıkkale Özelinde 2019-2024 Yerel Seçimleri̇ Düzeyinde Karşılaştırmalı Analiz. Balkanlar KAYFOR / Kamu Yönetiminde Güncel Tartışmalar, 166-169. Üsküp/Makedonya.`,
+        "link": "bildiriler/Yerel_Siyasetin_Esnaflasmasi_Kirikkale_O.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R., ve Aslan, F. D. (2023). Küresel Çevre Sorunları Karşısında Türkiye’nin Yüz Yıllık Yaklaşımı. Cumhuriyet, 483-485. Kırıkkale: Kırıkkale Üniversitesi.`,
+        "link": "bildiriler/Kuresel_Cevre_Sorunlari_Karsisinda_Turki.pdf"
+    },
+    {
+        "citation": `Yaylı, H., ve Yaslıkaya, R. (2019). Bir Komşuluk ve Yerleşme Birimi Olarak Türkiye’nin Şehirleşme Politikasında “Mahalle”nin Yeniden İnşası. Şehircilik ve Mutlu Şehir, 304-312. Kocaeli: Kocaeli Büyükşehir Belediyesi Yayını.`,
+        "link": "bildiriler/BIR_KOMSULUK_VE_YERLESME_BIRIMI_OLARAK_T.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R. (2018). Etimesgut Yerel Seçimlerinin Retrospektif Analizi. Uluslararası Tarihte Etimesgut Sempozyumu, 422-430. Ankara: Etimesgut Belediye Başkanlığı Kültür Yayınları.`,
+        "link": "bildiriler/etimesgut yerel seçimleri.pdf"
+    },
+    {
+        "citation": `Yaylı, H., ve Yaslıkaya, R. (2016). 15 Temmuz Darbe Girişimi’nin Belediyeler Üzerindeki Yansıması Hakkında Bir Değerlendirme. Giresun Üniversitesi Uluslararası Demokrasi Sempozyumu: Darbeler ve Tepkiler, 778-794. Giresun: Divan Kitap.`,
+        "link": "bildiriler/15_TEMMUZ_DARBE_GIRISIMI_NIN_BELEDIYELER.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R. (2016). Kentsel Yenileme mi? Soylulaştırma mı? 4. Uluslararası Kentsel ve Çevresel Sorunlar ve Politikalar Kongresi, 235-243.`,
+        "link": "bildiriler/kentsel yenileme soylulaştırma.pdf"
+    },
+    {
+        "citation": `Yaylı, H., ve Yaslıkaya, R. (2015). Enerji Üretiminde Yeni Bir Alternatif Olarak Yenilenebilir Enerji Kooperatifçiliği. 21. Milletlerarası Türk Kooperatifçilik Kongresi: Küreselleşme Sürecinde Ekonomi-Siyaset-Toplum ve Kooperatifçilik, 835-858. Ankara: KoopKur Yayını.`,
+        "link": "bildiriler/Enerji_Uretiminde_Yeni_Bir_Alternatif_Ol.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R., ve Yılmaz Ökde, F. (2015). Türkiye’de Büyükşehir Modeline Yönelik Eleştiriler: Yapısal-İşlevsel ve Tarihsel Bir İnceleme. Kamu Yönetimi Forumu (KAYFOR): Kamu Yönetiminde Değişimin Yönü ve Etkileri, 1528-1558. Konya.`,
+        "link": "bildiriler/büyükşehir modeline yönelik eleştiriler.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R. (2014). 6360 Sayılı Yasanın Uygulanmasında Yaşanan Sorunlar: Mersin Büyükşehir Belediyesi Örneği. Uluslararası Türk Dünyası Yerel Yönetimler Sempozyumu: Türk Dünyasında Yerel Yönetimlerin Sorunları, 221-229. Gazi Magusa- Kıbrıs.`,
+        "link": "bildiriler/6360_SAYILI_YASANIN_UYGULANMASINDA_YASAN.pdf"
+    },
+    {
+        "citation": `Yaslıkaya, R., ve Yücel Batmaz, N. (2014). Kent Hakkının Bilinirliği Üzerine Karşılaştırmalı Bir Analiz: Kırıkkale Üniversitesi—Gazi Üniversitesi Örneği. 2. Uluslararası Davraz Kongresi: Küresel Sorunlar ve Çözüm Arayışları, 1911-1831. Isparta: Süleyman Demirel Üniversitesi Yayını.`,
+        "link": "bildiriler/kent hakkının bilinirliği.pdf"
+    }
+];
