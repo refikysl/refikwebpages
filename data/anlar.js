c7abb89ae208a96d3c07e4787cc2d792c49d8858
refikysl <refik@kku.edu.tr>
2026-02-07 14:47:53 +0300

    Create anlar.js

---
+++ data/anlar.js
@@ -0,0 +1,78 @@
+const anlarData = [
+    {
+        "id": 8,
+        "src": "images/anlar/cd2e4c16-c3af-4815-aad0-51cf6c308e0b.jpg",
+        "title": "SBKY Öğrenci Topluluğuyla Medya Okur Yazarlığı Eğitiminden Bir Kare",
+        "date": "2025-12-11",
+        "description": "",
+        "width": 400,
+        "height": 300
+    },
+    {
+        "id": 7,
+        "src": "images/anlar/IMG_2483.jpg",
+        "title": "Kentsel Politikalar Dersinde Öğrencilerimiz Gerçek Parselasyon Haritaları ile Çalışırken",
+        "date": "2025-12-10",
+        "description": "",
+        "width": 400,
+        "height": 300
+    },
+    {
+        "id": 6,
+        "src": "images/anlar/IMG_2345.jpg",
+        "title": "Kahramanmaraş Elbistan'da Eski Öğrencilerimizden Yusuf'la",
+        "date": "2025-11-22",
+        "description": "",
+        "width": 400,
+        "height": 300
+    },
+    {
+        "id": 5,
+        "src": "images/anlar/9cfbd513-46f0-4f72-8789-e4d309bc407c.jpg",
+        "title": "Kahramanmaraş'ta Eski Öğrencilerimizden İdari Hakimlerimiz Osman Ceylan, Erdi Cenan ve Sevgili Eşi Şule ile Birlikte",
+        "date": "2025-11-21",
+        "description": "",
+        "width": 400,
+        "height": 600
+    },
+    {
+        "id": 4,
+        "src": "images/anlar/IMG_2477.jpg",
+        "title": "Kentsel Politikalar Dersinde Bekir ALTUNÇANAK (Kırıkkale Belediyesi Eski Fen İşleri Müdürü/Harita Mühendisi) İmar Yasası 18. Madde Uygulamasını Anlatıyor",
+        "date": "2025-12-10",
+        "description": "",
+        "width": 400,
+        "height": 600
+    },
+    {
+        "id": 3,
+        "src": "images/anlar/IMG_2551.jpg",
+        "title": "Karşılaştırmalı Kamu Yönetimi Dersinde Hamdi Turşucu Özbekistan Üzerine Konuşma Yapmak Üzere Dersimize Misafir Oldu",
+        "date": "2025-12-23",
+        "description": "",
+        "width": 400,
+        "height": 600
+    },
+    {
+        "id": 2,
+        "src": "images/anlar/IMG_1472.jpg",
+        "title": "KAYFOR Kongresi için Türkistan'da (Kazakistan) Hoca Ahmet Yesevi Türbesi Bahçesinde",
+        "date": "2025-09-11",
+        "description": "",
+        "width": 400,
+        "height": 300
+    },
+    {
+        "id": 1,
+        "src": "images/anlar/d6603221-1c24-4cd3-9bfa-926cd08bb81d.jpg",
+        "title": "Aksaray Üniversitesi öğrencilerine Tübitak 2209 Projeleri eğitimi verirken",
+        "date": "2025-10-31",
+        "description": "",
+        "width": 400,
+        "height": 300
+    }
+];
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = anlarData;
+}
